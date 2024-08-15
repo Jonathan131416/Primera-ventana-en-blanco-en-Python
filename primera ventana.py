7f69f9d3806725691799f16a340a276938182eaa
Jonathan131416 <jonathan.gonzalez23@unach.mx>
2024-08-14 21:52:41 -0600

    Add files via upload

---
+++ primera ventana.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+from tkinter import*
+raiz=Tk()
+raiz.title("primera ventana")
+raiz.geometry("520x480")
+raiz.mainloop()
